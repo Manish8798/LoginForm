54e7aa7d90434f795d47412f6058c88af37c5c3f
Manish8798 <mankum08071998@gmail.com>
2023-10-27 03:34:31 +0530

    navigation added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Form1 from './src/screens/Form1';
+import Form2 from './src/screens/Form2';
+import Form3 from './src/screens/Form3';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Form1" component={Form1} />
+        <Stack.Screen name="Form2" component={Form2} />
+        <Stack.Screen name="Form3" component={Form3} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
--- src/screens/Form1.js
+++ src/screens/Form1.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, Button} from 'react-native';
 
-const Form1 = () => {
+const Form1 = props => {
   return (
     <SafeAreaView>
       <Text>Form1</Text>
